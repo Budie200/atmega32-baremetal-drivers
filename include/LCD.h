d7df4ccd163cf5fdd6880dbfb5f7457443d3ac28
Abdullah Khataan <abdoallah.khataan@gmail.com>
2026-07-22 14:31:25 +0200

    feat: define params for lcd driver

---
+++ include/LCD.h
@@ -0,0 +1,19 @@
+#ifndef LCD_H
+#define LCD_H
+#include "EmbVars.h"
+
+typedef struct{
+    u8 DataPort;
+    u8 CtrlPort;
+    u8 Rs;
+    u8 Rw;
+    u8 E;
+} LCD_Config;
+
+void LCD_Pulse(LCD_Config *lcd);
+void LCD_SendCommand(LCD_Config *lcd , u8 command);
+void LCD_Init(LCD_Config *lcd);
+void LCD_Char(LCD_Config *lcd, u8 letter);
+void LCD_String(LCD_Config *lcd, const u8* letters);
+
+#endif
