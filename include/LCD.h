ecca7a51cd9c593ef85755ec77c7ee5ba04db012
Abdullah Khataan <abdoallah.khataan@gmail.com>
2026-07-23 23:09:13 +0200

    feat: implement 16x2 lcd functions.

--- include/LCD.h
+++ include/LCD.h
@@ -12,7 +12,7 @@ typedef struct{
 
 void LCD_Pulse(LCD_Config *lcd);
 void LCD_SendCommand(LCD_Config *lcd , u8 command);
-void LCD_Init(LCD_Config *lcd);
+void LCD_Init(LCD_Config *lcd); // allow options for init later
 void LCD_Char(LCD_Config *lcd, u8 letter);
 void LCD_String(LCD_Config *lcd, const u8* letters);
 void LCD_Clear(LCD_Config *lcd);
---
+++ src/LCD.C
@@ -0,0 +1,64 @@
+#include <util/delay.h>
+#include "EmbVars.h"
+#include "bitwise_ops.h"
+#include "DIO.h"
+#include "LCD.h"
+
+void LCD_Pulse(LCD_Config *lcd){
+	DIO_SetPinValue(lcd->CtrlPort, lcd->E, 1);
+	_delay_us(2);
+	DIO_SetPinValue(lcd->CtrlPort, lcd->E, 0);
+	_delay_us(2);	
+}
+
+void LCD_SendCommand(LCD_Config *lcd, u8 command){
+	DIO_SetPinValue(lcd->CtrlPort, lcd->Rs, 0);
+	DIO_SetPinValue(lcd->CtrlPort, lcd->Rw, 0);
+	DIO_SetPinValue(lcd->CtrlPort, lcd->E, 0);
+	
+	DIO_SetPortValue(lcd->DataPort, command);
+
+	LCD_Pulse(lcd);
+	
+}
+
+// allow options for init later
+void LCD_Init(LCD_Config *lcd){
+	_delay_ms(50);
+	LCD_SendCommand(lcd, 0b00111000);
+	_delay_us(40);
+	LCD_SendCommand(lcd, 0b00111000);
+	_delay_us(40);
+	LCD_SendCommand(lcd, 0b00001111);
+	_delay_us(40);
+	LCD_SendCommand(lcd, 0b00000001);
+	_delay_ms(2);
+	LCD_SendCommand(lcd, 0b00000110);
+}
+
+void LCD_Char(LCD_Config *lcd, u8 letter){
+	DIO_SetPinValue(lcd->CtrlPort, lcd->Rs, 1);
+	DIO_SetPinValue(lcd->CtrlPort, lcd->Rw, 0);
+	DIO_SetPinValue(lcd->CtrlPort, lcd->E, 0);
+
+	DIO_SetPortValue(lcd->DataPort, letter);
+	
+	LCD_Pulse(lcd);
+}
+
+void LCD_String(LCD_Config *lcd, const u8* letters){
+	for(int i = 0; letters[i] != '\0'; i++){
+		LCD_Char(lcd, letters[i]);
+	}
+}
+
+void LCD_Clear(LCD_Config *lcd){
+	DIO_SetPinValue(lcd->CtrlPort, lcd->Rs, 0);
+	DIO_SetPinValue(lcd->CtrlPort, lcd->Rw, 0);
+	DIO_SetPinValue(lcd->CtrlPort, lcd->E, 0);
+
+	DIO_SetPortValue(lcd->DataPort, 0b00000001);
+
+	LCD_Pulse(lcd);
+	_delay_ms(2);
+}
